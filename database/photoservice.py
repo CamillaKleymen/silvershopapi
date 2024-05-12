b2a73c5f21b7efb3a9bb860887333b22b6cbc7a8
CamillaKleymen <camillakleymen@outlook.com>
2024-05-12 17:15:48 +0500

    last edition

--- database/photoservice.py
+++ database/photoservice.py
@@ -2,9 +2,9 @@ from database import get_db
 from database.models import ProdPhoto
 from datetime import datetime
 
-def add_photo_db(post_id, photo_path):
+def add_photo_db(prod_id, photo_path):
     db = next(get_db())
-    photo = ProdPhoto(post_id=post_id, photo_path=photo_path, reg_date=datetime.now())
+    photo = ProdPhoto(product_id=prod_id, photo_path=photo_path, reg_date=datetime.now())
     db.add(photo)
     db.commit()
-    return post_id
+    return prod_id
